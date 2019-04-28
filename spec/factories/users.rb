840937b6c0851e979831629d4f161ca19d1ce80a
Andrew Mark <codcore@gmail.com>
2019-04-28 17:43:54 +0300

    Remove unnecessary id attribute from users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,12 +3,7 @@ FactoryBot.define do
     "user#{n}@test.com"
   end
 
-  sequence :id do |n|
-    n
-  end
-
   factory :user, aliases: [:author] do
-    id
     email
     password { 'password' }
     password_confirmation { 'password' }
