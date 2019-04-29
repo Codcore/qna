a57f3e2faaeac2ef2ec132478ef29771717d72ae
Andrew Mark <codcore@gmail.com>
2019-04-29 14:05:16 +0300

    Add not null constrains for the author_id for Question and Answer tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_28_134240) do
+ActiveRecord::Schema.define(version: 2019_04_29_105710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2019_04_28_134240) do
     t.bigint "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "author_id"
+    t.integer "author_id", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2019_04_28_134240) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id"
+    t.bigint "author_id", null: false
     t.index ["author_id"], name: "index_questions_on_author_id"
   end
 
