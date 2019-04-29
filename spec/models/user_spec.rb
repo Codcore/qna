4af0ef4e12c6fb24c647cec414ef5d9c687aaa76
Andrew Mark <codcore@gmail.com>
2019-04-29 14:15:42 +0300

    Add test for User#authorized_for?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { create(:user) }
+  let(:another_user) { create(:user) }
+  let(:question) { create(:question, author: user) }
+
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+  it 'should determine if user is an author of resource' do
+    expect(user.authorized_for?(question)).to eq true
+    expect(another_user.authorized_for?(question)).to eq false
+  end
 end
