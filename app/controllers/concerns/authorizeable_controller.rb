6e7c6e2f81a12ebecb72528093b5ef9407b3a06b
Andrew Mark <codcore@gmail.com>
2019-04-29 12:44:46 +0300

    Update tests for questions and answers controllers

--- app/controllers/concerns/authorizeable_controller.rb
+++ app/controllers/concerns/authorizeable_controller.rb
@@ -4,6 +4,6 @@ module AuthorizeableController
   extend ActiveSupport::Concern
 
   def authorize_author_for!(resource)
-    return render template: 'errors/401_error', status: :unauthorized unless resource.authorized?(current_user)
+    return render template: 'errors/403_error', status: :forbidden unless resource.authorized?(current_user)
   end
 end
