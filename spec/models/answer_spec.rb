daf897ed3b6d43b5a9daefb6f0e381094bbe728f
Andrew Mark <codcore@gmail.com>
2019-05-06 13:26:49 +0300

    Update answer_spec.rb

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -13,12 +13,12 @@ RSpec.describe Answer, type: :model do
   it '#best_solution! should set correctly answer as a best solution' do
     subject.best_solution!
 
-    expect(subject.best_solution).to eq true
+    expect(subject.best_solution).to be_truthy
   end
 
   it '#best_solution! should set correctly as a best solution only one answer of question' do
     subject.best_solution!
-    expect(another_answer.best_solution).to eq false
+    expect(another_answer.best_solution).to be_falsey
   end
 
   it '#best_solution! should re-set correctly another answer as a best solution' do
@@ -26,7 +26,7 @@ RSpec.describe Answer, type: :model do
     another_answer.best_solution!
     another_answer.reload
 
-    expect(another_answer.best_solution).to eq true
+    expect(another_answer.best_solution).to be_truthy
   end
 
   it '#best_solution! should set previous best solution answer as a non-best solution' do
@@ -34,6 +34,6 @@ RSpec.describe Answer, type: :model do
     another_answer.best_solution!
     subject.reload
 
-    expect(subject.best_solution).to eq false
+    expect(subject.best_solution).to be_falsey
   end
 end
