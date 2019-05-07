0de043b6bc76e69ae9b1597b413c463f62049065
Andrew Mark <codcore@gmail.com>
2019-05-07 18:25:10 +0300

    Author of question can delete attachments

--- app/controllers/concerns/attachable_resource.rb
+++ app/controllers/concerns/attachable_resource.rb
@@ -7,7 +7,7 @@ module AttachableResource
     if params[:purge_attachment_id]
       file = resource.files.find_by_id(params[:purge_attachment_id])
       file.purge
-      render 'shared/_delete_attachment.js.erb', locals: { file_id: file.id }
+      return render 'shared/_delete_attachment.js.erb', locals: { file_id: file.id }
     end
   end
 end
--- spec/features/question/delete_attached_files_spec.rb
+++ spec/features/question/delete_attached_files_spec.rb
@@ -1,26 +1,23 @@
 require 'rails_helper'
 
-feature 'Author can delete attached files for his answers', %q{
-  In order to remove attached files to answer
+feature 'Author can delete attached files for his questions', %q{
+  In order to remove attached files to questions
   As an author
   I'd like to delete them through delete button
 } do
 
   given(:author)   { create(:user) }
   given(:another_user)   { create(:user) }
-  given!(:question) { create(:question) }
-  given!(:answer_1) { create(:answer, :with_files, question: question, author: author) }
-  given!(:answer_2) { create(:answer, question: question) }
+  given!(:question) { create(:question, :with_files, author: author) }
 
-  scenario 'Author can delete attached files to his answer', js: true do
+  scenario 'Author can delete attached files to his question', js: true do
     sign_in author
     visit question_path(question)
 
-    within ".answers #answer-#{answer_1.id}" do
-      file_id = answer_1.files.last.id
+    within ".question" do
+      file_id = question.files.last.id
 
       expect(page).to have_link(class: 'delete-file-link', count: 1)
-
       accept_confirm do
         click_link(class: 'delete-file-link')
       end
@@ -34,7 +31,7 @@ feature 'Author can delete attached files for his answers', %q{
   scenario 'Non author cannot delete attachments' do
     visit question_path(question)
 
-    within ".answers #answer-#{answer_1.id}" do
+    within ".question" do
       expect(page).not_to have_link(class: 'delete-file-link')
     end
   end
@@ -43,7 +40,7 @@ feature 'Author can delete attached files for his answers', %q{
     sign_in another_user
     visit question_path(question)
 
-    within ".answers #answer-#{answer_1.id}" do
+    within ".question" do
       expect(page).not_to have_link(class: 'delete-file-link')
     end
   end
