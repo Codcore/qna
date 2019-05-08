42da86a249a6178229ba41b835475e1fc61c909b
Andrew Mark <codcore@gmail.com>
2019-05-08 23:47:31 +0300

    Move attachments deletion logic to the AttachmentsController

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'questions#index'
 
+  delete '/attachments/:id', to: 'attachments#destroy', as: 'attachment_delete'
+
   resources :questions do
     resources :answers, shallow: true, except: [:index, :show, :new, :edit] do
       member do
