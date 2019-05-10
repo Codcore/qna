3357cff1fdf3d0e0c1e2b6e9df6ea7366b4defa3
Andrew Mark <codcore@gmail.com>
2019-05-11 00:36:35 +0300

    Refactor feature tests for link adding for answers

--- spec/features/answers/add_links_spec.rb
+++ spec/features/answers/add_links_spec.rb
@@ -14,11 +14,11 @@ feature 'User can add links to the answer', %q{
     sign_in user
     visit question_path(question)
     click_on 'add link'
+    fill_in I18n.translate('helpers.label.answer.body'), with: 'Test answer'
+    fill_in I18n.translate('helpers.label.link.name'), with: 'My link'
   end
 
   scenario 'User adds link when asks answer with GitHub Gist url', js: true do
-    fill_in I18n.translate('helpers.label.answer.body'), with: 'Test answer'
-    fill_in I18n.translate('helpers.label.link.name'), with: 'My gist'
     fill_in I18n.translate('helpers.label.link.url'), with: gist_url
 
     click_on I18n.translate('helpers.submit.answer.create')
@@ -27,18 +27,15 @@ feature 'User can add links to the answer', %q{
   end
 
   scenario 'User adds link and links form resets', js: true do
-    fill_in I18n.translate('helpers.label.answer.body'), with: 'Test answer'
-    fill_in I18n.translate('helpers.label.link.name'), with: 'My gist'
     fill_in I18n.translate('helpers.label.link.url'), with: 'http://google.com'
 
     click_on I18n.translate('helpers.submit.answer.create')
 
-    expect(page).to have_link('add link')
+    expect(page).not_to have_content(I18n.translate('helpers.label.link.name'))
+    expect(page).not_to have_content(I18n.translate('helpers.label.link.url'))
   end
 
   scenario 'User adds link when asks answer with invalid url', js: true do
-    fill_in I18n.translate('helpers.label.answer.body'), with: 'Test answer'
-    fill_in I18n.translate('helpers.label.link.name'), with: 'My gist'
     fill_in I18n.translate('helpers.label.link.url'), with: 'some_url'
 
     click_on I18n.translate('helpers.submit.answer.create')
