d7172fc0c818ffab4a7a096faa3db31cf0524de4
Andrew Mark <codcore@gmail.com>
2019-05-31 12:28:11 +0300

    Finish Oauth wit Github

---
+++ app/models/authorization.rb
@@ -0,0 +1,4 @@
+class Authorization < ApplicationRecord
+  belongs_to :user
+  validates :uid, :provider, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_22_122312) do
+ActiveRecord::Schema.define(version: 2019_05_30_133702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,16 @@ ActiveRecord::Schema.define(version: 2019_05_22_122312) do
     t.integer "author_id", null: false
     t.boolean "best_solution", default: false
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "authorizations", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_authorizations_on_provider_and_uid"
+    t.index ["user_id"], name: "index_authorizations_on_user_id"
   end
 
   create_table "commentaries", force: :cascade do |t|
@@ -109,6 +119,7 @@ ActiveRecord::Schema.define(version: 2019_05_22_122312) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "answers", "questions"
+  add_foreign_key "authorizations", "users"
   add_foreign_key "commentaries", "users"
   add_foreign_key "questions", "users", column: "author_id"
   add_foreign_key "rewards", "questions"
